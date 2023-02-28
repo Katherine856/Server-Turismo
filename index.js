c925ff49862cfc60679bbc0ab12f9a7a4cd42e62
Katherine856 <kathyuska97@gmail.com>
2023-02-28 17:27:39 -0500

    Avance 28/02

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-console.log("holi");
+const express = require("express");
+const cors = require('cors');
+const connection = require("./conexion/Conectar");
+const app = express();
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+const port = 5000;
+
+app.get("/servicio/:id", function (req, res) {
+  console.log("ok");
+  //connection.connect();
+  
+  const articleId = req.params.id;
+
+  connection.query(`
+  SELECT Id_Servicio, N_Servicio, N_Empresa, V_Min_Servicio, V_Max_Servicio, D_Servicio, F_Empresa, I_Empresa, W_Empresa, T_Empresa, C_Empresa D_Empresa, C_T_Servicio 
+  FROM Servicio, Empresa, Tipo_servicio 
+  WHERE Empresa.Id_Empresa=Servicio.Id_Empresa AND Servicio.Id_T_Servicio=Tipo_Servicio.Id_T_Servicio AND Id_Servicio=${articleId};
+  `, (err, result, fields) => {
+    if (!err) {
+      console.log(result);
+      res.send(result[0]);
+    } else {
+      throw err;
+    }
+  });
+  //connection.end();
+});
+
+app.get("/comida", function (req, res) {
+  console.log("ok");
+
+  connection.query(`
+  SELECT Id_Servicio, N_Servicio, Empresa.N_Empresa, V_Min_Servicio, V_Max_Servicio 
+  FROM Servicio, Empresa 
+  WHERE Empresa.Id_Empresa=Servicio.Id_Empresa AND Servicio.Id_T_Servicio=1;
+  `, (err, result, fields) => {
+    if (!err) {
+      console.log(result);
+      res.send(result);
+    } else {
+      throw err;
+    }
+  });
+});
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+});
